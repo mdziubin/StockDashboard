b7e02e67294f2d74d5e84163b7f98dbd49eb1133
mdziubin <mdziubin@ualberta.ca>
2020-10-13 20:32:42 -0600

    Added delete stock concept

--- src/views/reports/DashboardView/FavTable.js
+++ src/views/reports/DashboardView/FavTable.js
@@ -7,13 +7,17 @@ import {
   Card,
   CardHeader,
   Divider,
+  IconButton,
   Table,
   TableBody,
   TableCell,
   TableHead,
-  TableRow
+  TableRow,
+  FormControlLabel,
+  Switch
 } from '@material-ui/core';
 import ArrowRightIcon from '@material-ui/icons/ArrowRight';
+import DeleteIcon from '@material-ui/icons/Delete';
 import StyledTC from './StyledTC';
 import { getFavs, getPrices } from '../../../services/stock-service';
 import CContentLoading from '../../../components/CContentLoading';
@@ -24,6 +28,8 @@ const FavTable = () => {
   const [stocks, setStocks] = useState([]);
   const [isLoading, setLoading] = useState(true);
   const [err, setError] = useState(false);
+
+  const [edit, setEdit] = useState(false);
 
   useEffect(() => {
     loadData()
@@ -36,6 +42,16 @@ const FavTable = () => {
     if (stockArray.length === 0) return;
     const info = await getPrices(stockArray);
     setStocks(info);
+  };
+
+  const editSwitchHandler = event => {
+    setEdit(event.target.checked);
+  };
+
+  const deleteStockHandler = (symbol, i) => {
+    let newArr = [...stocks];
+    newArr.splice(i, 1);
+    setStocks(newArr);
   };
 
   let body = <CContentLoading err={err} />;
@@ -53,23 +69,37 @@ const FavTable = () => {
                   <TableCell>Last Price</TableCell>
                   <TableCell>Change</TableCell>
                   <TableCell>% Change</TableCell>
+                  {edit && <TableCell></TableCell>}
                 </TableRow>
               </TableHead>
               <TableBody>
-                {stocks.map(({ quote }) => (
+                {stocks.map(({ quote }, i) => (
                   <TableRow hover key={quote.symbol}>
                     <TableCell>{quote.symbol}</TableCell>
                     <TableCell>{quote.companyName}</TableCell>
                     <TableCell>{quote.latestPrice}</TableCell>
                     <StyledTC val={quote.change} icon />
                     <StyledTC val={quote.changePercent} percent />
+                    {edit && (
+                      <TableCell>
+                        <IconButton
+                          onClick={() => deleteStockHandler(quote.symbol, i)}
+                        >
+                          <DeleteIcon fontSize="small" color="action" />
+                        </IconButton>
+                      </TableCell>
+                    )}
                   </TableRow>
                 ))}
               </TableBody>
             </Table>
           </Box>
         </PerfectScrollbar>
-        <Box display="flex" justifyContent="flex-end" p={2}>
+        <Box display="flex" justifyContent="space-between" p={2}>
+          <FormControlLabel
+            control={<Switch checked={edit} onChange={editSwitchHandler} />}
+            label="Edit"
+          />
           <Button
             color="primary"
             endIcon={<ArrowRightIcon />}
